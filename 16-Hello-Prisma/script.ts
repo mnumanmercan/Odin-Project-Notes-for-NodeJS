6cd67568053d7964fe6ac0af1bb59503e31ada0a
Numan <numan.mercan24@gmail.com>
2026-01-14 23:18:41 +0300

    feat: new migration and prisma client practice

--- 16-Hello-Prisma/script.ts
+++ 16-Hello-Prisma/script.ts
@@ -2,23 +2,17 @@ import { prisma } from "./lib/prisma";
 
 async function main() {
   // Create a new user with a post
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-      posts: {
-        create: {
-          title: "Hello World",
-          content: "This is my first post!",
-          published: true,
-        },
-      },
-    },
-    include: {
-      posts: true,
-    },
+  const newPost = await prisma.user.createMany({
+    data: [
+      { email: "123@gmail.com", name: "Test1" },
+      { email: "345@gmail.com", name: "Test2" },
+      { email: "678@gmail.com", name: "Test3" },
+      { email: "910@gmail.com", name: "Test4" },
+
+    ],
+    skipDuplicates: true
   });
-  console.log("Created user:", user);
+  console.log(newPost);
 
   // Fetch all users with their posts
   const allUsers = await prisma.user.findMany({
@@ -27,6 +21,13 @@ async function main() {
     },
   });
   console.log("All users:", JSON.stringify(allUsers, null, 2));
+
+  const specificUser = await prisma.user.findUnique({
+    where: {
+        email: "123@gmail.com"
+    }
+  });
+  console.log("Target user:", specificUser);
 }
 
 main()
