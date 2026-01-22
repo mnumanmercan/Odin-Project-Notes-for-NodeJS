06a1b1a648b52c9adc4ff5221f4f3c4bab89db09
Numan <numan.mercan24@gmail.com>
2026-01-22 23:36:27 +0300

    new: Learning practice for JWT token

---
+++ 19-API_Sec/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api' , (req, res) => {
+    res.json({
+        message: 'Welcome to the API',
+    })
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created',
+                authData
+            })
+        }
+    })
+});
+
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'exampleUser',
+        email: 'example@example.com'
+    };
+
+    jwt.sign({ user }, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+
+    if (typeof bearerHeader !== 'undefined') {
+        const bearerToken = bearerHeader.split(' ')[1];
+
+        req.token = bearerToken;
+
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
